c093ff467ae4988a5608404fcf4a6e625a4628cd
Anthony <anthony@routestack.net>
2025-10-30 17:25:42 +0000

    Checking if requester is VIP

--- app/sql/requesters_db.py
+++ app/sql/requesters_db.py
@@ -8,6 +8,7 @@ def create_requesters_table():
             cur.execute("""CREATE TABLE IF NOT EXISTS requesters (
                 id NUMERIC PRIMARY KEY,
                 is_agent BOOLEAN,
+                vip BOOLEAN,
                 first_name TEXT,
                 last_name TEXT,
                 primary_email TEXT,
@@ -19,6 +20,7 @@ def create_requesters_table():
 def add_requesters_table(requester):
     id = requester.get("id")
     is_agent = requester.get("is_agent")
+    vip = requester.get("vip_user")
     first_name = requester.get("first_name")
     last_name = requester.get("last_name")
     primary_email = requester.get("primary_email")
@@ -30,9 +32,10 @@ def add_requesters_table(requester):
     with psycopg.connect(db) as conn:
         with conn.cursor() as cur:
             try:
-                cur.execute("""INSERT INTO requesters (id, is_agent, first_name, last_name, primary_email, work_phone_number, mobile_phone_number, department_ids, reporting_manager_id)
-                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
-                            (id, is_agent, first_name, last_name, primary_email, work_phone_number, mobile_phone_number, department_ids, reporting_manager_id))
+                cur.execute("""INSERT INTO requesters 
+                            (id, is_agent, vip, first_name, last_name, primary_email, work_phone_number, mobile_phone_number, department_ids, reporting_manager_id)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+                            (id, is_agent, vip, first_name, last_name, primary_email, work_phone_number, mobile_phone_number, department_ids, reporting_manager_id))
             except psycopg.errors.UniqueViolation:
                 return
 
