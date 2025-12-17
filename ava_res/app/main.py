694428a3559e5c6c276f148319eb5ff0ca9d2b69
Anthony Figueroa <afigueroa@eastwest.com>
2025-12-17 23:42:04 +0000

    Moved Ava summarizer to it's own script
    - Moved over Ava summarizer to allow for that script to always be
      working on ticket summarization and not be slowed down marking tickets
      as closed/resoved or any other status
    - Restored TC back to how it was prior

---
+++ ava_res/app/main.py
@@ -0,0 +1,63 @@
+from app.freshservice.conversations import get_ticket_conversations
+from app.sql.tickets import query_open_tickets, update_ticket_table, query_one_ticket
+from app.freshservice.tickets import get_one_ticket
+from app.ai.resolution_note import ai_resolution_note
+from app.argparse import parse_args
+from app.logs import logs
+
+import time, sys
+
+def main() -> None:
+    args = parse_args()
+
+    arg_id = args.id if args.id else None
+
+    while True:
+        if arg_id:
+            ticket = query_one_ticket(arg_id)
+            if ticket:
+                logs(f"Starting test run on ticket ID# {arg_id}")
+                tickets = [ticket]
+            else:
+                logs(f"Could not run test on ticket ID# {arg_id}! Could not find in DB")
+                sys.exit(1) 
+        else:
+            tickets = query_open_tickets()
+
+        if not tickets:
+            logs("All tickets have either been updating or do not require updating at this time.")
+            logs("Sleeping for 5 minutes")
+
+            #5 Minutes
+            time.sleep(300)
+
+        for ticket in tickets:
+            if arg_id:
+                id = ticket.get("id")
+            else:
+                id = ticket[0]
+
+            ticket = get_one_ticket(id)
+            
+            if not ticket:
+                continue
+
+            conversation = get_ticket_conversations(ticket)
+
+            update_ticket_table(ticket, conversation)
+            
+            status = ticket.get("status")
+            if status == 5:
+                ai_resolution_note(ticket)
+
+            if arg_id:
+                logs(f"Ending test run on ticket ID# {id}")
+                sys.exit(0)
+            else:
+                time.sleep(1)
+
+def runner() -> None:
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nExiting...")
--- tc/app/main.py
+++ tc/app/main.py
@@ -1,28 +1,14 @@
 from app.freshservice.conversations import get_ticket_conversations
-from app.sql.tickets import query_open_tickets, update_ticket_table, query_one_ticket
+from app.sql.tickets import query_open_tickets, update_ticket_table
 from app.freshservice.tickets import get_one_ticket
-from app.ai.resolution_note import ai_resolution_note
-from app.argparse import parse_args
 from app.logs import logs
 
-import time, sys
+import time
 
 def main() -> None:
-    args = parse_args()
-
-    arg_id = args.id if args.id else None
-
     while True:
-        if arg_id:
-            ticket = query_one_ticket(arg_id)
-            if ticket:
-                logs(f"Starting test run on ticket ID# {arg_id}")
-                tickets = [ticket]
-            else:
-                logs(f"Could not run test on ticket ID# {arg_id}! Could not find in DB")
-                sys.exit(1) 
-        else:
-            tickets = query_open_tickets()
+
+        tickets = query_open_tickets()
 
         if not tickets:
             logs("All tickets have either been updating or do not require updating at this time.")
@@ -32,10 +18,7 @@ def main() -> None:
             time.sleep(300)
 
         for ticket in tickets:
-            if arg_id:
-                id = ticket.get("id")
-            else:
-                id = ticket[0]
+            id = ticket[0]
 
             ticket = get_one_ticket(id)
             
@@ -46,15 +29,7 @@ def main() -> None:
 
             update_ticket_table(ticket, conversation)
             
-            status = ticket.get("status")
-            if status == 5:
-                ai_resolution_note(ticket)
-
-            if arg_id:
-                logs(f"Ending test run on ticket ID# {id}")
-                sys.exit(0)
-            else:
-                time.sleep(3)
+            time.sleep(1)
 
 def runner() -> None:
     try:
